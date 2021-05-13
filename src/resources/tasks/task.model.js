f7cfdeda0973a5e8fcd1a7081cd4b5279b429cdd
DeHoKo <zenkovsk@gmail.com>
2021-05-13 16:46:35 +0300

    feat: implement task model

--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -1,1 +1,23 @@
+const { v4: uuid } = require('uuid');
 
+class Task {
+  constructor({
+    id = uuid(),
+    title = 'New task',
+    order,
+    description = 'A new task',
+    userId,
+    boardId,
+    columnId,
+  }) {
+    this.id = id;
+    this.title = title;
+    this.order = order;
+    this.description = description;
+    this.userId = userId;
+    this.boardId = boardId;
+    this.columnId = columnId;
+  }
+}
+
+module.exports = Task;
