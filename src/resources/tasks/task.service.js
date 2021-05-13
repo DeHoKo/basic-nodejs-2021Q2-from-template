0f1dbc93047880f9cc354d3ac1a1abbdf381d870
DeHoKo <zenkovsk@gmail.com>
2021-05-13 16:47:29 +0300

    feat: implement task service

--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,1 +1,32 @@
+const tasksRepo = require('./task.memory.repository');
+const TaskModel = require('./task.model');
 
+const getTasksByBoardId = (boardId) => tasksRepo.getTasksByBoardId(boardId);
+
+const getTasksByUserId = (userId) => tasksRepo.getTasksByUserId(userId);
+
+const getTaskById = (taskId) => tasksRepo.getTaskById(taskId);
+
+
+const createTask = (taskData, boardId) => {
+  const task = new TaskModel({ ...taskData, boardId });
+
+  return tasksRepo.createTask(task);
+};
+
+const updateTask = (taskData) => {
+  const task = new TaskModel(taskData);
+
+  return tasksRepo.updateTask(task);
+};
+
+const deleteTask = (taskId) => tasksRepo.deleteTask(taskId);
+
+module.exports = {
+  getTasksByBoardId,
+  getTasksByUserId,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+};
